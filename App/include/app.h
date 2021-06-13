f0313b5df3ffb7b689eb520c1f39a7cf6a54ef44
Joel Jassan <joeljassan@hotmail.com>
2021-06-13 01:22:07 -0300

    Inicializacion del proyecto

---
+++ App/include/app.h
@@ -0,0 +1,17 @@
+/*
+ * app.h
+ *
+ *  Created on: Jun 13, 2021
+ *      Author: joelj
+ */
+
+#ifndef INCLUDE_APP_H_
+#define INCLUDE_APP_H_
+
+void setup(void); //declaro una funcion visible globalmente, que no tiene argumentos y no devuelve nada
+void loop(void);
+
+
+//app.h indica la interfaz para poder usar lo que este en app.c  ----> entonces llevo el app.h al app.c incluyendolo como libreria...
+
+#endif /* INCLUDE_APP_H_ */
---
+++ App/source/app.c
@@ -0,0 +1,16 @@
+/*
+ * app.c
+ *
+ *  Created on: Jun 13, 2021
+ *      Author: joelj
+ */
+
+
+#include <app.h>
+#include <main.h>
+
+void setup(void){
+
+}
+void loop(void){
+}
